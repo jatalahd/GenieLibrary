05ebe4ae6855c5364cce425d1aa7a11c28ea917b
jatalahd <jatalahd@gmail.com>
2014-01-27 19:23:26 +0200

    added keywords based on GenieComponent, GenieTextInput and GenieDisplayObject

--- src/main/java/org/robotframework/genielibrary/GenieKeywords.java
+++ src/main/java/org/robotframework/genielibrary/GenieKeywords.java
@@ -7,8 +7,14 @@ import org.robotframework.javalib.annotation.RobotKeywordOverload;
 
 import com.adobe.genie.executor.Genie;
 import com.adobe.genie.executor.LogConfig;
-import com.adobe.genie.executor.components.GenieMovieClip;
+import com.adobe.genie.executor.components.GenieComponent;
+import com.adobe.genie.executor.components.GenieDisplayObject;
+import com.adobe.genie.executor.components.GenieTextInput;
+//import com.adobe.genie.executor.uiEvents.UIGenieID;
+//import com.adobe.genie.executor.uiEvents.UIKeyBoard;
 import com.adobe.genie.genieCom.SWFApp;
+
+import java.awt.Point;
 
 
 @RobotKeywords
@@ -96,57 +102,178 @@ public class GenieKeywords {
         }
     }
 
-    @RobotKeyword("Clicks a GenieMovieClip object with a given genieID.\n\n"
+    @RobotKeyword("Clicks a GenieComponent object with a given genieID.\n\n"
                    + "Example:\n"
-                   + "| ClickGenieMovieClipObject | genieID |\n")
+                   + "| ClickGenieComponent | genieID |\n")
     @ArgumentNames({"genieID"})
-    public void clickGenieMovieClipObject(final String genieID) throws Exception {
+    public void clickGenieComponent(final String genieID) throws Exception {
         try {
-            (new GenieMovieClip(genieID, app)).waitFor(this.waitTimeout);
-            (new GenieMovieClip(genieID, app)).click();
+            (new GenieComponent(genieID,app)).waitFor(this.waitTimeout);
+            (new GenieComponent(genieID,app)).click();
         } catch (Exception e) {
-            throw new StepFailed("Error: Click on genieID " + genieID + " failed!");
+            throw new StepFailed("Error: Click on GenieComponent " + genieID + " failed!");
         }
     }
 
-    @RobotKeyword("Reads and returns the text of the given GenieMovieClip object.\n\n"
+    @RobotKeyword("Reads and returns the text of the given GenieComponent object.\n\n"
                    + "Example:\n"
-                   + "| ReadGenieMovieClipObject | genieID |\n")
+                   + "| ${text}= | ReadGenieComponent | genieID |\n")
     @ArgumentNames({"genieID"})
-    public String readGenieMovieClipObject(final String genieID) throws Exception {
+    public String readGenieComponent(final String genieID) throws Exception {
         boolean bVisible=false;
         String retVal = "";
         try {
-            (new GenieMovieClip(genieID, app)).waitFor(this.waitTimeout);
-            bVisible = (new GenieMovieClip(genieID, app)).isVisible();
-            if (bVisible) { retVal = (new GenieMovieClip(genieID, app)).getValueOf("text"); }
+            (new GenieComponent(genieID,app)).waitFor(this.waitTimeout);
+            bVisible = (new GenieComponent(genieID,app)).isVisible();
+            if (bVisible) { retVal = (new GenieComponent(genieID,app)).getValueOf("text"); }
         } catch (Exception e) {
-            throw new StepFailed("Error: reading text from genieID " + genieID + " failed!");
+            throw new StepFailed("Error: reading text from GenieComponent " + genieID + " failed!");
         }
         return retVal;
     }
 
-    @RobotKeyword("Waits for the given time for genieID object to have a property with given value.\n\n"
+    @RobotKeyword("Waits for the given time for given GenieComponent property to obtain the given value.\n\n"
                    + "Examples:\n"
-                   + "| WaitGenieMovieClipProperty | genieID | visible | true  | 5  |\n"
-                   + "| WaitGenieMovieClipProperty | genieID | visible | false | 10 |\n")
+                   + "| WaitForGenieComponentProperty | genieID | visible | true          | 5  |\n"
+                   + "| WaitForGenieComponentProperty | genieID | enabled | true          | 3  |\n"
+                   + "| WaitForGenieComponentProperty | genieID | text    | dynamic_text  | 6  |\n"
+                   + "| WaitForGenieComponentProperty | genieID | visible | false         | 10 |\n")
     @ArgumentNames({"genieID","property","value","timeout"})
-    public void waitGenieMovieClipProperty(final String genieID, final String prop, 
-                                           final String val, final String tout) throws Exception {
+    public void waitForGenieComponentProperty(final String genieID, final String prop, 
+                                              final String val, final String tout) throws Exception {
         boolean outcome = false;
         try {
-            if (new GenieMovieClip(genieID, app).waitForPropertyValue(prop,val,Integer.parseInt(tout))) {
-                outcome = true;
-            }
+            outcome = (new GenieComponent(genieID,app)).waitForPropertyValue(prop,val,Integer.parseInt(tout));
         } catch (Exception e) {
-            throw new StepFailed("Error: wait for property of genieID " + genieID + " failed!");
+            throw new StepFailed("Error: error occurred when waiting GenieComponent " 
+                                  + genieID + " to have property " + prop + " with value " + val);
         }
         if (!outcome) {
-            throw new StepFailed("Error: timeout " + tout + " seconds when waiting genieID " 
+            throw new StepFailed("Error: timeout " + tout + " seconds when waiting GenieComponent " 
                                   + genieID + " to have property " + prop + " with value " + val);
         }
     }
     
+    @RobotKeyword("Takes a screenshot of the given GenieComponent object "
+                   + "and saves it as a .png file to the given directory.\n\n"
+                   + "Examples:\n"
+                   + "| CaptureGenieComponentImage | genieID | ./myImage.png         |\n"
+                   + "| CaptureGenieComponentImage | genieID | C:\\myfolder\\img.png |\n")
+    @ArgumentNames({"genieID","imagePath"})
+    public void captureGenieComponentImage(final String genieID, final String imagePath) throws Exception {
+        try {
+            (new GenieComponent(genieID,app)).captureComponentImage(imagePath);
+        } catch (Exception e) {
+            throw new StepFailed("Error: screenshot of GenieComponent " + genieID + " failed!");
+        }
+    }
+
+    @RobotKeyword("Returns the local coordinates of the given GenieComponent object.\n\n"
+                   + "Example:\n"
+                   + "| ${coords}= | GetGenieLocalCoordinates | genieID |\n")
+    @ArgumentNames({"genieID"})
+    public String getGenieLocalCoordinates(final String genieID) throws Exception {
+        Point xy = new Point();
+        try {
+            xy = (new GenieComponent(genieID,app)).getLocalCoordinates();
+        } catch (Exception e) {
+            throw new StepFailed("Error: getting local coordinates of GenieComponent " + genieID + " failed!");
+        }
+        return xy.toString();
+    }
+
+    @RobotKeyword("Writes text to the given GenieTextInput object.\n\n"
+                   + "Example:\n"
+                   + "| WriteToGenieTextInput | genieID | some_text |\n")
+    @ArgumentNames({"genieID","text"})
+    public void writeToGenieTextInput(final String genieID, final String text) throws Exception {
+        try {
+            (new GenieTextInput(genieID,app)).waitFor(this.waitTimeout);
+            (new GenieTextInput(genieID,app)).input(text,"false");
+        } catch (Exception e) {
+            throw new StepFailed("Error: writing text to GenieTextInput " + genieID + " failed!");
+        }
+    }
+
+    @RobotKeyword("Presses a key on the given GenieTextInput object.\n\n"
+                   + "Examples:\n"
+                   + "| PressKeyOnGenieTextInput | genieID | ENTER  |\n"
+                   + "| PressKeyOnGenieTextInput | genieID | ESCAPE |\n")
+    @ArgumentNames({"genieID","key"})
+    public void pressKeyOnGenieTextInput(final String genieID, final String key) throws Exception {
+        try {
+            (new GenieTextInput(genieID,app)).waitFor(this.waitTimeout);
+            (new GenieTextInput(genieID,app)).input(key,"false");
+        } catch (Exception e) {
+            throw new StepFailed("Error: pressing key on GenieTextInput " + genieID + " failed!");
+        }
+    }
+
+    @RobotKeyword("Executes a double click on the given GenieTextInput object.\n\n"
+                   + "Example:\n"
+                   + "| DoubleClickOnGenieTextInput | genieID |\n")
+    @ArgumentNames({"genieID"})
+    public void doubleClickOnGenieTextInput(final String genieID) throws Exception {
+        try {
+            (new GenieTextInput(genieID,app)).waitFor(this.waitTimeout);
+            (new GenieTextInput(genieID,app)).doubleClick();
+        } catch (Exception e) {
+            throw new StepFailed("Error: double click on GenieTextInput " + genieID + " failed!");
+        }
+    }
+
+    @RobotKeyword("Executes a native mouse click on the given GenieDisplayObject. "
+                   + "Only accepts click parameters of the format: int1,int2,int3,int4,int5,int6,int7,boolean "
+                   + "where the last boolean argument can have values 'true' or 'false'. "
+                   + "If 'true' is given, the mouse is physically moved to the location of the click.\n\n"
+                   + "Example:\n"
+                   + "| ClickGenieDisplayObject | genieID | 69,20,298,122,575,360,3,false |\n")
+    @ArgumentNames({"genieID","clickArgumentString"})
+    public void clickGenieDisplayObject(final String genieID, final String args) throws Exception {
+        String[] buf = args.split(","); 
+        boolean bool = buf[(buf.length - 1)].equals("true");
+        try {
+            (new GenieDisplayObject(genieID,app)).waitFor(this.waitTimeout);
+            (new GenieDisplayObject(genieID,app)).click(Integer.parseInt(buf[0]),
+                                                        Integer.parseInt(buf[1]),
+                                                        Integer.parseInt(buf[2]),
+                                                        Integer.parseInt(buf[3]),
+                                                        Integer.parseInt(buf[4]),
+                                                        Integer.parseInt(buf[5]),
+                                                        Integer.parseInt(buf[6]), bool );
+        } catch (Exception e) {
+            throw new StepFailed("Error: native click on GenieTextInput " + genieID + " failed!");
+        }
+    }
+
+    @RobotKeyword("Executes a double click on the given GenieDisplayObject. "
+                   + "The x and y arguments define the coordinates to click upon.\n\n"
+                   + "Example:\n"
+                   + "| DoubleClickOnGenieDisplayObject | genieID | x | y |\n")
+    @ArgumentNames({"genieID"})
+    public void doubleClickOnGenieDisplayObject(final String genieID, final String x, final String y) throws Exception {
+        try {
+            (new GenieDisplayObject(genieID,app)).waitFor(this.waitTimeout);
+            (new GenieDisplayObject(genieID,app)).doubleClick(Integer.parseInt(x),Integer.parseInt(y));
+        } catch (Exception e) {
+            throw new StepFailed("Error: double click on GenieDisplayObject " + genieID + " failed!");
+        }
+    }
+
+    @RobotKeyword("Executes a native key press action on the given GenieDisplayObject. "
+                   + "The duration of the keypress is hardcoded as 0.5 seconds.\n\n"
+                   + "Examples:\n"
+                   + "| PressKeyOnGenieDisplayObject | genieID | UP   |\n"
+                   + "| PressKeyOnGenieDisplayObject | genieID | DOWN |\n")
+    @ArgumentNames({"genieID","key"})
+    public void pressKeyOnGenieDisplayObject(final String genieID, final String key) throws Exception {
+        try {
+            (new GenieDisplayObject(genieID,app)).waitFor(this.waitTimeout);
+            (new GenieDisplayObject(genieID,app)).performKeyAction(key,500);
+        } catch (Exception e) {
+            throw new StepFailed("Error: pressing key " + key + " on GenieDisplayObject " + genieID + " failed!");
+        }
+    }
 
 
 } // End Of GenieKeywords
